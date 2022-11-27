7b2a44e0264085c391081a7ce9e35d51c72722da
gabiswar <108243030+gabiswar@users.noreply.github.com>
2022-11-27 12:20:28 -0300

    Create conexao.php

---
+++ conexao.php
@@ -0,0 +1,13 @@
+<?php
+
+$hostname = "localhost";
+$db = "teste1";
+$user = "root";
+$pass = "";
+
+$mysqli = new mysqli($hostname, $user, $pass, $db);
+
+if($mysqli->connect_errno) {
+    echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
+
