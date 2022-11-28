e658d30f2c65fd00d66e54867b625110ec8c7da1
Rafael <rafaellima2k02@gmail.com>
2022-11-28 18:00:34 -0300

    telaLogin

--- conexao.php
+++ conexao.php
@@ -10,4 +10,4 @@ $mysqli = new mysqli($hostname, $user, $pass, $db);
 if($mysqli->connect_errno) {
     echo "Falha ao conectar: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
-
+    
