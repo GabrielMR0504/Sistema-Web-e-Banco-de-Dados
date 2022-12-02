8214a8bd944ad9bd7cfd40a4b4284df370fff86c
Rafael <rafaellima2k02@gmail.com>
2022-12-01 23:26:57 -0300

    mudançaPagamento

--- pagamento.php
+++ pagamento.php
@@ -1,24 +1,24 @@
 <?php
-	include ('protect.php');
-  include('conexao.php');
+include('protect.php');
+include('conexao.php');
 //echo $_SESSION['nome'];
-if(isset($_POST['tipo']) || isset($_POST['nome']) || isset($_POST['numero']) || isset($_POST['dataExp']) || isset($_POST['cvv']) || isset($_POST['cpf'])) {
-        //limpando as variavei
-        $tipo = $mysqli->real_escape_string($_POST['tipo']);
-        $nome = $mysqli->real_escape_string($_POST['nome']);
-        $numero = $mysqli->real_escape_string($_POST['numero']);
-        $dataexp = $mysqli->real_escape_string($_POST['dataexp']);
-        $cvv = $mysqli->real_escape_string($_POST['cvv']);
-        $cpf = $_SESSION['cpf'];
-        $sql_code = $mysqli->prepare("INSERT INTO `formapagamento` (`Tipo`, `Nome`, `Numero`, `DataExp`, `CVV`) VALUES (?,?,?,?,?)");
-        $sql_code->bind_param("ssssss", $tipo, $nome, $numero, $dataexp, $cvv);
-        $sql_code->execute(); 
+if (isset($_POST['tipo']) || isset($_POST['nome']) || isset($_POST['numero']) || isset($_POST['dataExp']) || isset($_POST['cvv']) || isset($_POST['cpf'])) {
+  //limpando as variavei
+  $tipo = $mysqli->real_escape_string($_POST['tipo']);
+  $nome = $mysqli->real_escape_string($_POST['nome']);
+  $numero = $mysqli->real_escape_string($_POST['numero']);
+  $dataexp = $mysqli->real_escape_string($_POST['dataexp']);
+  $cvv = $mysqli->real_escape_string($_POST['cvv']);
+  $cpf = $_SESSION['cpf'];
+  $sql_code = $mysqli->prepare("INSERT INTO `formapagamento` (`Tipo`, `Nome`, `Numero`, `DataExp`, `CVV`) VALUES (?,?,?,?,?)");
+  $sql_code->bind_param("ssssss", $tipo, $nome, $numero, $dataexp, $cvv);
+  $sql_code->execute();
 
-        //$sql_codeVenda = $mysqli->prepare("UPDATE TABLE `venda` SET (`Tipo`, `Nome`, `Numero`, `DataExp`, `CVV`) VALUES (?,?,?,?,?)");
-        //$sql_code->bind_param("ssssss", $tipo, $nome, $numero, $dataexp, $cvv);
-        //echo "Registro concluído";
-        //$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
-        header("Location: carrinho.php");
+  //$sql_codeVenda = $mysqli->prepare("UPDATE TABLE `venda` SET (`Tipo`, `Nome`, `Numero`, `DataExp`, `CVV`) VALUES (?,?,?,?,?)");
+  //$sql_code->bind_param("ssssss", $tipo, $nome, $numero, $dataexp, $cvv);
+  //echo "Registro concluído";
+  //$sql_query = $mysqli->query($sql_code) or die("Falha na execução do código SQL: " . $mysqli->error);
+  header("Location: carrinho.php");
 }
 ?>
 
@@ -37,60 +37,62 @@ if(isset($_POST['tipo']) || isset($_POST['nome']) || isset($_POST['numero']) || 
 </head>
 
 <body>
-<nav class="navbar navbar-expand-md bg-dark navbar-dark">
-        <!-- Brand -->
-        <a class="navbar-brand" href="#">
-            <img src="logo-portal.png" alt="Logo" style="width:60px;">
-        </a>
-        <!-- Navbar links -->
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-            <ul class="navbar-nav">
-            </ul>
-        </div>
-        <!-- Toggler/collapsibe Button -->
-    </nav>
-
-
-  <form action = "" method = "POST">
-    <div class="dropdown">
-      <h4 class="mb-3">Pagamento</h4>
-      <select name="tipo">
-        <option value="debito">Cartão de debito</option>
-        <option value="credito" selected>Cartão de Crédito</option>
-      </select>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="#">
+      <img src="logo-portal.png" alt="Logo" style="width:60px;">
+    </a>
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav">
+      </ul>
     </div>
-    <div class="col-md-6 mb-3">
-      <label for="cc-nome">Nome no cartão</label>
-      <input type="text" name="nome" class="form-control" id="cc-nome" placeholder="" required="">
-      <small class="text-muted">Nome completo, como mostrado no cartão.</small>
-      <div class="invalid-feedback">
-        O nome que está no cartão é obrigatório.
+    <!-- Toggler/collapsibe Button -->
+  </nav>
+
+  <div style="margin-left: 10px;">
+    <form action="" method="POST">
+      <div class="dropdown">
+        <h4 class="mb-3">Pagamento</h4>
+        <select name="tipo">
+          <option value="debito">Cartão de debito</option>
+          <option value="credito" selected>Cartão de Crédito</option>
+        </select>
       </div>
-    </div>
-    <div class="col-md-6 mb-3">
-      <label for="cc-numero">Número do cartão de crédito</label>
-      <input type="text" name="numero" class="form-control" id="cc-numero" placeholder="" required="">
-
-    </div>
-    <div class="row">
-      <div class="col-md-3 mb-3">
-        <label for="cc-expiracao">Data de expiração</label>
-        <input type="text" name="dataexp" class="form-control" id="cc-expiracao" placeholder="" required="">
+      <div class="col-md-6 mb-3">
+        <label for="cc-nome">Nome no cartão</label>
+        <input type="text" name="nome" class="form-control" id="cc-nome" placeholder="" required="">
+        <small class="text-muted">Nome completo, como mostrado no cartão.</small>
         <div class="invalid-feedback">
-          Data de expiração é obrigatória.
+          O nome que está no cartão é obrigatório.
         </div>
       </div>
-      <div class="col-md-3 mb-3">
-        <label for="cc-cvv">CVV</label>
-        <input type="text" name="cvv" class="form-control" id="cc-cvv" placeholder="" required="">
-        <div class="invalid-feedback">
-          Código de segurança é obrigatório.
-        </div>
+      <div class="col-md-6 mb-3">
+        <label for="cc-numero">Número do cartão de crédito</label>
+        <input type="text" name="numero" class="form-control" id="cc-numero" placeholder="" required="">
+
       </div>
-      <hr class="mb-4">
-            <button class="btn btn-primary btn-lg btn-block" type="submit">Finalizar compra</button>
-    </div>
-  </form>
+      <div class="row">
+        <div class="col-md-3 mb-3">
+          <label for="cc-expiracao">Data de expiração</label>
+          <input type="text" name="dataexp" class="form-control" id="cc-expiracao" placeholder="" required="">
+          <div class="invalid-feedback">
+            Data de expiração é obrigatória.
+          </div>
+        </div>
+        <div class="col-md-3 mb-3">
+          <label for="cc-cvv">CVV</label>
+          <input type="text" name="cvv" class="form-control" id="cc-cvv" placeholder="" required="">
+          <div class="invalid-feedback">
+            Código de segurança é obrigatório.
+          </div>
+        </div>
+        <hr class="mb-4">
+        <button class="btn btn-primary btn-lg btn-block" type="submit" style="background-color: #ec600f;">Finalizar compra</button>
+      </div>
+    </form>
+  </div>
+
 </body>
 
 </html>
